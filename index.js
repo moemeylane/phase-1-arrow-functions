6ec8074d059b54672af34b838180ff2c80c07655
moemeylane <wangarimaureen668@gmail.com>
2024-07-02 01:29:32 +0300

    complete lab

---
+++ index.js
@@ -0,0 +1,18 @@
+
+function divide() {
+    const num1 = 2000;
+    const num2 = 100;
+    return num1 / num2;
+}
+const square = (pm) => {
+    return pm * pm;
+};
+const add = (pr , pm)=> {
+    return pr + pm;
+}
+
+
+
+
+
+
